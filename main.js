f5a08bba4c98df146d59a1eb0a9e7561ebac1300
l4nk332 <l4nk332@gmail.com>
2017-06-05 22:37:29 -0700

    Refactor parser into function and set up file/dir recognition.

--- main.js
+++ main.js
@@ -7,24 +7,40 @@ const hsl = require("./color_regexes").hsl;
 
 let entryPath = process.argv[2];
 
-const rl = readline.createInterface({
-    input: fs.createReadStream(entryPath)
-});
+const parseFile = (filePath) => {
+    const rl = readline.createInterface({
+        input: fs.createReadStream(filePath)
+    });
 
-let lineCount = 1;
+    let lineCount = 1;
 
-rl.on('line', (line) => {
-    if (hex(line)) {
-        console.log(`${lineCount}: ${hex(line)}`);
+    rl.on('line', (line) => {
+        if (hex(line)) {
+            console.log(`${lineCount}: ${hex(line)}`);
+        }
+
+        if (rgb(line)) {
+            console.log(`${lineCount}: ${rgb(line)}`);
+        }
+
+        if (hsl(line)) {
+            console.log(`${lineCount}: ${hsl(line)}`);
+        }
+
+        lineCount++;
+    });
+};
+
+fs.lstat(entryPath, (err, stats) => {
+    if (err) {
+        console.error(err);
     }
 
-    if (rgb(line)) {
-        console.log(`${lineCount}: ${rgb(line)}`);
+    if (stats.isFile()) {
+        parseFile(entryPath);
     }
 
-    if (hsl(line)) {
-        console.log(`${lineCount}: ${hsl(line)}`);
+    if (stats.isDirectory()) {
+        console.log('This is a directory...');
     }
-
-    lineCount++;
 });
