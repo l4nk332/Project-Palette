afa3f4dcd377f3cce894d9f5b6fe38f72293669e
l4nk332 <l4nk332@gmail.com>
2017-06-19 22:55:05 -0700

    Add additional markup and styling to illustrate basic functionality.

--- markup-generator.js
+++ markup-generator.js
@@ -1,8 +1,21 @@
 const styles = `
+    html, body {
+        margin: 0;
+        padding: 0;
+    }
+
+    h5 {
+        text-align: center;
+    }
+
+    .swatch-container {
+        margin: 20px;
+    }
+
     .swatch {
         width: 200px;
         height: 200px;
-        margin: 20px;
+        margin: 20px auto;
     }
 
     .pallete {
@@ -11,11 +24,33 @@ const styles = `
         align-items: flex-start;
         justify-content: space-evenly;
     }
+
+    ul {
+        padding: 0;
+        margin: 0;
+        list-style: none;
+    }
 `.trim();
+
+const generateColorLocationInfo = (colorLocations) => {
+    return colorLocations.map(location => {
+        return `
+            <li>
+                <code>${location.filePath}: ${location.lineNumber}</code>
+            </li>
+        `;
+    }).join("");
+};
 
 const generateColorSwatches = (colorMap) => {
     return Object.keys(colorMap).map(color => {
-        return `<div class='swatch' style='background: ${color}'></div>`;
+        return `
+            <div class='swatch-container'>
+                <h5>${color}</h5>
+                <div class='swatch' style='background: ${color}'></div>
+                <ul>${generateColorLocationInfo(colorMap[color])}</ul>
+            </div>
+        `;
     }).join("");
 };
 
