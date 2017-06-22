2fd8d1a4417afa2c655761f07be9f62ed51da162
l4nk332 <l4nk332@gmail.com>
2017-06-21 22:24:31 -0700

    Fix both Sass and React portaions of setup.

--- src/react/app.jsx
+++ src/react/app.jsx
@@ -4,52 +4,52 @@ import ReactDOM from "react-dom";
 import colorPalette from "../color-palette.json";
 
 
-export default class App extends React.Component {
-    render() {
-        let colorSwatches = Object.keys(colorPalette).map(color => {
-            return (
-                <ColorSwatch color={color} locations={colorPalette[color]} />
-            );
-        });
-
-        return (
-            <div>
-                <div style={{ textAlign: 'center' }}>
-                    <input type='color' />
-                <div>
-                <div className='palette'></div>
-            </div>
-        );
-    }
+const ColorLocation = ({filePath, lineNumber}) => {
+	return (
+		<li>
+			<a href={"file://" + filePath} target="_blank">
+				<code>{filePath}: {lineNumber}</code>
+			</a>
+		</li>
+	);
 }
 
 const ColorSwatch = ({color, locations}) => {
-    let colorLocationInfo = locations.map(location => {
-        return (
-            <ColorLocation
-                filePath={location.filePath}
-                lineNumber={location.lineNumber}
-            />
-        );
-    });
+	let colorLocationInfo = locations.map(location => {
+		return (
+			<ColorLocation
+				filePath={location.filePath}
+				lineNumber={location.lineNumber}
+			/>
+		);
+	});
 
-    return (
-        <div className="swatch-container">
-            <h5>{color}</h5>
-            <div className="swatch" style={{ background: color }}></div>
-            <ul></ul>
-        </div>
-    );
-};
-
-const ColorLocation = ({filePath, lineNumber}) => {
-    return (
-        <li>
-            <a href={"file://" + filePath} target="_blank">
-                <code>{filePath}: {lineNumber}</code>
-            </a>
-        </li>
-    );
+	return (
+		<div className="swatch-container">
+			<h5>{color}</h5>
+			<div className="swatch" style={{ background: color }}></div>
+			<ul>{colorLocationInfo}</ul>
+		</div>
+	);
 }
 
-ReactDOM.mount(<App />, document.body);
+class App extends React.Component {
+	render() {
+		let colorSwatches = Object.keys(colorPalette).map(color => {
+			return (
+				<ColorSwatch color={color} locations={colorPalette[color]} />
+			);
+		});
+
+		return (
+			<div>
+				<div style={{ textAlign: 'center' }}>
+					<input type='color' />
+				</div>
+				<div className="palette">{colorSwatches}</div>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.body);
