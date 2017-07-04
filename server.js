fb697afd276f9a7f00b551655cf8a59cd934958d
l4nk332 <l4nk332@gmail.com>
2017-07-04 12:32:32 -0700

    Begin adding logic to parse url and run unix cmds.

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
-require('dotenv').config();
+const util = require("util");
+const childProcess = require("child_process");
+const exec = util.promisify(childProcess.exec)
+
+require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-app.use(express.static('./dist'));
+app.use(express.static("./dist"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get("/", (req, res) => {
-    res.render('index.html');
+    res.render("index.html");
+});
+
+app.post("/colors", (req, res) => {
+    const repoUrl = req.body.repoUrl;
+    exec("mkdir ./temp/" + repoUrl)
+        .then((output) => {
+            res.send("Folder successfully created...");
+        })
+        .catch((error) => {
+            res.send(error);
+        });
 });
 
 const SERVER_PORT = process.env.SERVER_PORT;
