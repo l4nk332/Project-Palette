bbb08da07f671a4c9ef78b1f4b5fbce64d30e80c
l4nk332 <l4nk332@gmail.com>
2017-07-09 13:45:53 -0700

    Remove unnecessary icon components.

--- src/react/components/ColorSwatchControls.jsx
+++ src/react/components/ColorSwatchControls.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 
-import FullscreenToggleControl from "./FullscreenToggleControl.jsx";
 import FillColorControl from "./FillColorControl.jsx";
 import ColorizeControl from "./ColorizeControl.jsx";
-import RevertColorControl from "./RevertColorControl.jsx";
-import RemoveColorControl from "./RemoveColorControl.jsx";
 
 function ColorSwatchControls() {
     return (
         <div>
-            <FullscreenToggleControl isActive={false} />
             <FillColorControl />
             <ColorizeControl />
-            <RevertColorControl />
-            <RemoveColorControl />
         </div>
     );
 }
