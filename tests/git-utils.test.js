e52bc9349b1641f59727242eaaee3cd89d533207
l4nk332 <l4nk332@gmail.com>
2017-07-25 22:55:53 -0700

    Add tests around making sure that the hashes produced are unique.

--- tests/git-utils.test.js
+++ tests/git-utils.test.js
@@ -8,3 +8,14 @@ test("Should properly normalize a GitHub url", () => {
     expect(normalized.hashedUri === normalized.repoUri).toBeFalsy();
     expect(normalized.hashedUri === normalized.uniqueHash).toBeFalsy();
 });
+
+test("Should produce unique hashes for each function call", () => {
+    let url = "https://github.com/l4nk332/ianjabour.io";
+    let uniqueHashes = [10];
+
+    for (let i = 0; i < 1000; i++) {
+        let hash = normalizeGitHubUrl(url);
+        expect(uniqueHashes.includes(hash)).toBeFalsy();
+        uniqueHashes.push(hash);
+    }
+});
