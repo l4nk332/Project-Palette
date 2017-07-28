d76805bfcc5391a634e2359961d9d4e101a76cd0
l4nk332 <l4nk332@gmail.com>
2017-07-28 07:05:10 -0700

    Completes passing test for palette parsing of basic path.

---
+++ tests/basic_path/script.js
@@ -0,0 +1,13 @@
+$(".blah").css({
+    backgroundColor: "rgb(123, 123, 231)"
+});
+
+$("#special").css({
+    color: "rgba(89, 12, 31, 0.1)",
+    backgroundColor: "hsl(300, 12%, 31%)"
+})
+
+$(".warning").css({
+    color: "rgba(89, 12, 31, 0.1)",
+    border-color: "hsla(300, 12%, 31%, 0.3)"
+})
