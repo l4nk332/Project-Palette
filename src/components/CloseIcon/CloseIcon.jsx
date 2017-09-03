778ce5d108d39e4fa1b78a3a69880de44fb45c26
Ian Jabour <l4nk332@gmail.com>
2017-09-03 00:35:13 -0700

    Create CloseIcon component.

---
+++ src/components/CloseIcon/CloseIcon.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import TiTimes from 'react-icons/lib/ti/times'
+
+import './CloseIcon.sass'
+
+function CloseIcon({handleClick}) {
+  return (
+    <TiTimes
+      className='CloseIcon'
+      onClick={() => {handleClick()}}
+    />
+  )
+}
+
+export default CloseIcon
