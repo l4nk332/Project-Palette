1710ee848aef75198eb24458ea9ec755862c6944
Ian Jabour <l4nk332@gmail.com>
2017-12-12 06:45:46 -0800

    Begin refactor the imports to use newly created index files at module levels
    The webpack config and eslintrc have been adjusted to allow for this
    new way of importing to occur.

---
+++ src/components/index.js
@@ -0,0 +1,55 @@
+import Anchor from './Anchor/Anchor';
+import Button from './Button/Button';
+import CloseIcon from './CloseIcon/CloseIcon';
+import ColorSwatch from './ColorSwatch/ColorSwatch';
+import Error from './Error/Error';
+import FixedWrapper from './FixedWrapper/FixedWrapper';
+import FlexWrapper from './FlexWrapper/FlexWrapper';
+import Form from './Form/Form';
+import Grid from './Grid/Grid';
+import Heading from './Heading/Heading';
+import IconAssistedField from './IconAssistedField/IconAssistedField';
+import Jumbotron from './Jumbotron/Jumbotron';
+import Lead from './Lead/Lead';
+import Loader from './Loader/Loader';
+import LoadingWrapper from './LoadingWrapper/LoadingWrapper';
+import LocationList from './LocationList/LocationList';
+import Navbar from './Navbar/Navbar';
+import Paragraph from './Paragraph/Paragraph';
+import Rule from './Rule/Rule';
+import SegmentRule from './SegmentRule/SegmentRule';
+import SelectField from './SelectField/SelectField';
+import SplitButton from './SplitButton/SplitButton';
+import SplitView from './SplitView/SplitView';
+import TextField from './TextField/TextField';
+import Title from './Title/Title';
+import Toggleable from './Toggleable/Toggleable';
+
+export {
+  Anchor,
+  Button,
+  CloseIcon,
+  ColorSwatch,
+  Error,
+  FixedWrapper,
+  FlexWrapper,
+  Form,
+  Grid,
+  Heading,
+  IconAssistedField,
+  Jumbotron,
+  Lead,
+  Loader,
+  LoadingWrapper,
+  LocationList,
+  Navbar,
+  Paragraph,
+  Rule,
+  SegmentRule,
+  SelectField,
+  SplitButton,
+  SplitView,
+  TextField,
+  Title,
+  Toggleable,
+};
---
+++ src/containers/index.js
@@ -0,0 +1,11 @@
+import AppContainer from './AppContainer';
+import DetailContainer from './DetailContainer';
+import PaletteContainer from './PaletteContainer';
+import SearchContainer from './SearchContainer';
+
+export {
+  AppContainer,
+  DetailContainer,
+  PaletteContainer,
+  SearchContainer,
+};
