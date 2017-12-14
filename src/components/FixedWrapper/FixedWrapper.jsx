2948b39e1fdc5e40655935ef654cf07f1dc2a29b
Ian Jabour <l4nk332@gmail.com>
2017-12-14 06:22:20 -0800

    Fix all the imports within components/ folder to use absolute imports.

--- src/components/FixedWrapper/FixedWrapper.jsx
+++ src/components/FixedWrapper/FixedWrapper.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {getTextColor} from '../../utils/color-manipulation';
+import {getTextColor} from 'utils/color-manipulation';
 
 import './FixedWrapper.sass';
 
