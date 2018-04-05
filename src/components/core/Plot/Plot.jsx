c29bd86615acc6e2ca02d0977d13f3ce1b922f8d
Ian Jabour <l4nk332@gmail.com>
2018-04-04 22:51:15 -0700

    Refactor Plot to use svg for dynamic sizing and adjust basic positioning.

--- src/components/core/Plot/Plot.jsx
+++ src/components/core/Plot/Plot.jsx
@@ -1,56 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 
-import { ColorSpectrum } from 'components';
+import {ColorSpectrum} from 'components';
 
 import s from './Plot.sass';
 
-const Plot = ({
-  xScale,
-  yScale,
-  xAxis,
-  yAxis,
-  points,
-}) => (
-  <main
-    className={s.container}
-    style={{width: `300px`, height: `300px`, margin: '0 0 42px 42px'}}
-  >
-
-    <section
-      style={{
-        display: 'flex',
-        flexFlow: 'column nowrap',
-        alignItems: 'flex-end',
-        justifyContent: 'space-between',
-        height: '100%',
-        padding: '0 1em',
-        position: 'absolute',
-        left: '-2.7em',
-        bottom: '0',
-      }}
-    >
+const Plot = ({points}) => (
+  <main className={s.container}>
+    <section className={s.yAxis}>
       <span>5</span>
       <span>4</span>
       <span>3</span>
       <span>2</span>
       <span>1</span>
     </section>
-
-    {points.map((point, idx) => (
-      <span
-        title={point.color}
-        className={s.point}
-        style={{
-          backgroundColor: point.color,
-          padding: point.size || '0.4em',
-          bottom: `calc(${point.y * 3}px - ${point.size || '0.4em'})`,
-          left: `calc(${point.x * 3}px - ${point.size || '0.4em'})`,
-        }}
-        key={idx}
-      />
-    ))}
+    <svg className={s.points} viewBox="0 0 100 100">
+      {points.map((point, idx) => (
+        <circle
+          cx={point.x}
+          cy={point.y}
+          className={s.point}
+          fill={point.color}
+          r="3"
+          key={idx}
+        />
+      ))}
+    </svg>
     <ColorSpectrum className={s.spectrum} />
   </main>
 );
@@ -66,8 +41,6 @@ Plot.propTypes = {
     color: PropTypes.string.isRequired,
     size: PropTypes.string,
   })),
-  xScale: PropTypes.number.isRequired,
-  yScale: PropTypes.number.isRequired,
 };
 
 export default Plot;
