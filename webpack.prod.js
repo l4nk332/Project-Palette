32a5f70cfe1b2dc53aa8682b8a6d271a8ab3deb4
Ian Jabour <l4nk332@gmail.com>
2018-06-01 08:03:10 -0700

    Update yarn build script to disable bundle analysis server

--- webpack.prod.js
+++ webpack.prod.js
@@ -24,6 +24,7 @@ module.exports = merge(common, {
       deleteOriginalAssets: true,
     }),
     new BundleAnalyzerPlugin({
+      analyzerMode: 'disabled',
       generateStatsFile: true,
       openAnalyzer: false,
     }),
