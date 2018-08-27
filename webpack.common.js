5bf5ebaa212750270657aa341c793922870e5f56
Ian Jabour <l4nk332@gmail.com>
2018-08-26 21:27:53 -0700

    Add package for bundling favicon with webpack along with favicon png.

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 const devMode = process.env.NODE_ENV !== 'production';
 
@@ -70,6 +71,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
+    new FaviconsWebpackPlugin('./logo.png'),
   ],
   output: {
     filename: '[name].[chunkhash].bundle.js',
