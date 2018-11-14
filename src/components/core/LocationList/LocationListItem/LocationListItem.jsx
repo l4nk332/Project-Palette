dcb7d3981ac615af8a6a929551fc87d368a54ec1
Ian Jabour <l4nk332@gmail.com>
2018-11-13 19:25:41 -0800

    Touch up ui around listing out the line location links for a given color.

--- src/components/core/LocationList/LocationListItem/LocationListItem.jsx
+++ src/components/core/LocationList/LocationListItem/LocationListItem.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import {triggerIfEnterKey, openNewTab} from 'utils/misc';
 
 import s from './LocationListItem.sass';
 
@@ -8,16 +11,28 @@ const LocationListItem = ({
   lineNumber,
   projectUrl,
   defaultBranch,
-}) => (
-  <a
-    href={`${projectUrl}/blob/${defaultBranch}/${filePath}#L${lineNumber}`}
-    className={s.container}
-    target="_blank"
-  >
-    <div className={s.path}>{filePath}</div>
-    :{lineNumber}
-  </a>
-);
+  isDark,
+}) => {
+  const linkToLine = () => openNewTab(
+    `${projectUrl}/blob/${defaultBranch}/${filePath}#L${lineNumber}`,
+  );
+
+  return (
+    <div
+      className={classNames(s.container, isDark ? s.dark : s.light)}
+      onClick={linkToLine}
+      onKeyDown={event => {
+        triggerIfEnterKey(event, linkToLine);
+      }}
+      tabIndex={0}
+      role="link"
+    >
+      <span className={s.path}>
+        {`${filePath}:${lineNumber}`}
+      </span>
+    </div>
+  );
+};
 
 LocationListItem.propTypes = {
   filePath: PropTypes.string.isRequired,
@@ -27,6 +42,11 @@ LocationListItem.propTypes = {
   ]).isRequired,
   projectUrl: PropTypes.string.isRequired,
   defaultBranch: PropTypes.string.isRequired,
+  isDark: PropTypes.bool,
+};
+
+LocationListItem.defaultProps = {
+  isDark: false,
 };
 
 export default LocationListItem;
